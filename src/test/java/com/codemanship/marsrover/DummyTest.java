ab85d37ca269005801f98c1a17b82dda61d21f1d
Jason Gorman <jason.gorman@codemanship.com>
2025-05-29 08:58:02 +0100

    Walking skeleton - dummy test + Hello, World! CLI output

---
+++ src/test/java/com/codemanship/marsrover/DummyTest.java
@@ -0,0 +1,12 @@
+package com.codemanship.marsrover;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DummyTest {
+    @Test
+    void dummy(){
+        assertTrue(true);
+    }
+}
