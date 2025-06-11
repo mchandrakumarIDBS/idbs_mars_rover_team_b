6581ed435643f5d247b3a796ea025a5466bfc09f
Manoj Chandrakumar <mchandrakumar@idbs.com>
2025-06-11 12:14:05 +0100

    Created the Rover class and written a test

---
+++ src/main/java/com/codemanship/marsrover/Rover.java
@@ -0,0 +1,13 @@
+package com.codemanship.marsrover;
+
+public class Rover {
+    private int x;
+
+    public Rover(int x) {
+        this.x = x;
+    }
+
+    public int getX() {
+        return x;
+    }
+}
---
+++ src/test/java/com/codemanship/marsrover/RoverTest.java
@@ -0,0 +1,15 @@
+package com.codemanship.marsrover;
+
+import org.junit.jupiter.api.Test;
+
+import java.awt.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class RoverTest {
+    @Test
+    void testRoverCanBeDropped(){
+        Rover rover = new Rover(5);
+        assertEquals(5, rover.getX());
+    }
+}
